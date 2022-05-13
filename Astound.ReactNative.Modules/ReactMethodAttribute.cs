109d510388a2603e69f38d9d14521b14de945e53
Kenny McClive <kmcclive@captechconsulting.com>
2022-05-13 10:25:21 -0600

    Added documentation to attributes.

--- Astound.ReactNative.Modules/ReactMethodAttribute.cs
+++ Astound.ReactNative.Modules/ReactMethodAttribute.cs
@@ -2,16 +2,29 @@
 
 namespace Astound.ReactNative.Modules;
 
+/// <summary>
+/// An attribute that marks whether a method should be exported to JavaScript.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class ReactMethodAttribute : Attribute
 {
-    public string Name { get; set; }
+    /// <summary>
+    /// The name to use when calling the method in JavaScript.
+    /// </summary>
+    public string? Name { get; set; }
 
+    /// <summary>
+    /// Creates a new <see cref="ReactMethodAttribute"/>.
+    /// </summary>
     public ReactMethodAttribute()
     {
 
     }
 
+    /// <summary>
+    /// Creates a new <see cref="ReactMethodAttribute"/> with a specified name.
+    /// </summary>
+    /// <param name="name">The name to use when calling the method in JavaScript.</param>
     public ReactMethodAttribute(string name)
     {
         Name = name;
--- Astound.ReactNative.Modules/ReactModuleAttribute.cs
+++ Astound.ReactNative.Modules/ReactModuleAttribute.cs
@@ -2,17 +2,34 @@
 
 namespace Astound.ReactNative.Modules;
 
+/// <summary>
+/// An attribute that marks whether a class should be exported as a module to JavaScript.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class ReactModuleAttribute : Attribute
 {
-    public string Name { get; set; }
+    /// <summary>
+    /// The name to use when referencing the module in JavaScript.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Indicates whether the module requires main-queue setup.
+    /// </summary>
     public bool RequiresMainQueueSetup { get; set; }
 
+    /// <summary>
+    /// Creates a new <see cref="ReactModuleAttribute"/>.
+    /// </summary>
     public ReactModuleAttribute()
     {
 
     }
 
+    /// <summary>
+    /// Creates a new <see cref="ReactModuleAttribute"/> with a specified name.
+    /// </summary>
+    /// <param name="name">The name to use when referencing the module in JavaScript.</param>
     public ReactModuleAttribute(string name)
     {
         Name = name;
